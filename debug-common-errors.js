deae0f1dfa05396db9848da9d165a5fcc346f2e3
Romina Diaz-Rivero <romina@wustl.edu>
2025-09-03 20:30:20 -0500

    Did the debugging assignment

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,25 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
-
+// Syntax error.  At the end of the statement, it was missing a ", ), and ;.  So I added "); to the end
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+// let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+// Runtime error. The third line is trying to multiply each item in the numbers array, but the third item is a string, not a number, so it runs into an error
+// So I fixed it from turning "eight" to 8.
 
 
 // Program C (Logic Error)
@@ -48,15 +50,18 @@ for (let i = 0; i < numbers.length; i++) {
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
 function isPrime(num) {
-  if (num < 2) return false;
+  if (num < 2) return true;
   for (let i = 2; i < num; i++) {
+    console.log(`${num} divided by ${i} has a remainder of ${num % i }`);
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+// Logic error. The "return true" and "return false" should be switched, because the for loop is checking if the num can be divided by any number besides one, which if
+// true, then it's NOT a prime number. Else if nothing work, then it IS a prime number, which should return true.
